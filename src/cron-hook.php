dd4090fe3ce04e21328c041a042638d77b253824
Dimitar Dzhongov <djongov@gamerz-bg.com>
2022-11-28 23:34:21 +0200

    Update
    Cron webhook now active
    Converter now renames the file without the old extension in the name
    Removed unneeded web.config info from old template

--- src/cron-hook.php
+++ src/cron-hook.php
@@ -3,7 +3,10 @@ if (isset($_POST['delete']) and $_POST['delete'] === 'all') {
     $files = glob('./temp/*'); // get all file names
     foreach($files as $file){ // iterate files
         if(is_file($file)) {
-            unlink($file); // delete file
+            if (unlink($file)) {
+                echo 'Success - ' . implode(',', $_POST);
+            }
+            
         }
     }
 }
--- src/process-image.php
+++ src/process-image.php
@@ -83,7 +83,8 @@ do {
     if (imagewebp($img, $targetDirectory . '/' . $newName, 80)) {
         echo "<p>Successfully converted</p>";
         $old_webp_filepath = $targetDirectory . '/' . $newName;
-        $new_webp_filepath = $targetDirectory . '/' . basename($_FILES["image"]["name"], $extension) . '.webp';
+        $extension = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+        $new_webp_filepath = $targetDirectory . '/' . basename($_FILES["image"]["name"], $extension) . 'webp';
         $renamed_webp = rename($old_webp_filepath, $new_webp_filepath);
         $new_fileSize = filesize($new_webp_filepath);
         $new_fileSize_in_KBs = $new_fileSize / 1000;
@@ -94,7 +95,7 @@ do {
         } else {
             echo '<p>You saved ' . $saved_kbs . ' KB which is <strong>' . round(($fileSize_in_KBs / $new_fileSize_in_KBs) * 100) . '% savings</strong></p>';
         }
-        echo '<p>Download the converted webp <a class="underline text-green-500 hover:text-green-600 font-semibold" href="./temp/' . basename($_FILES["image"]["name"], $extension) . '.webp' . '" target="_blank">file</a></p>';
+        echo '<p>Download the converted webp <a class="underline text-green-500 hover:text-green-600 font-semibold" href="./temp/' . $new_webp_filepath . '" target="_blank">file</a></p>';
         echo '<p>You have 5 minutes to download it, then it disappears</p>';
     } else {
         echo '<p class="' . $error_class . '">Conversion unsuccessful</p>';
