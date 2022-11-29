3689df08290b8123a4a6c8ca2957671f91788a76
Dimitar Dzhongov <49032971+Djongov@users.noreply.github.com>
2022-11-29 11:45:03 +0200

    Update cron-hook.php

--- src/cron-hook.php
+++ src/cron-hook.php
@@ -1,13 +1,17 @@
 <?php
 if (isset($_POST['delete']) and $_POST['delete'] === 'all') {
     $files = glob('./temp/*'); // get all file names
+    $deleted_files = [];
     foreach($files as $file){ // iterate files
         if(is_file($file)) {
             if (unlink($file)) {
-                echo 'Success - ' . implode(',', $_POST);
+                array_push($deleted_files, $file);
             }
-            
         }
+    }
+    echo 'Deleted ' . count($deleted_files);
+    if (isset($_SERVER['HTTP_CRONHOOKS_SIGNATURE'])) {
+        echo '\n' .$_SERVER['HTTP_CRONHOOKS_SIGNATURE'];
     }
 }
 ?>
